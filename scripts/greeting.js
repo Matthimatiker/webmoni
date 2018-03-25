12c70f9ebe57fc02998058d7b394c2a55b2189bd
Matthias Molitor <matthias@molitor.solutions>
2018-03-25 23:23:00 +0200

    changed greeting, again

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -2,6 +2,6 @@
 
 module.exports = function(robot) {
   robot.respond(/Hi.*/, {}, (response) => {
-    response.send("Hi, Webmontag, ich habe mich leicht verspätet! Gnarf...");
+    response.send("Hi, Webmontag, ich habe mich leicht verspätet!");
   });
 };
